97139b5a05224506ec4c3fb0e8627a907d8a416e
asd35084591 <70816236+asd35084591@users.noreply.github.com>
2022-04-05 01:05:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,37 +2,45 @@ const express = require("express");
 const app = express();
 const http = require("http")
 const fs = require("fs")
-//const cp = require('child_process');
-const process = require("process")
+const os = require("os")
+const YTDlpWrap = require('yt-dlp-wrap').default;
 const server = http.createServer(app).listen(process.argv[2] || process.env.PORT || 9999,()=>{console.log(`http://localhost:${process.argv[2] || process.env.PORT ||  9999}`)})
 const open = require('open');
 const io = require('socket.io')(server)
-const yt_dlp = "yt-dlp/yt-dlp.exe"
-const YTDlpWrap = require('yt-dlp-wrap').default;
+require('dotenv').config();
+fs.access(process.env.ytdlp_path || (os.platform()==="linux"?"yt-dlp":"yt-dlp.exe"), fs.F_OK, async(err) => {
+    if (err) {
+      //console.error(err)
+      await YTDlpWrap.downloadFromGithub();
+      return
+    }
+    //file exists
+  })
+const yt_dlp = process.env.ytdlp_path || (os.platform()==="linux"?"yt-dlp":"yt-dlp.exe")
+const downloadpath =process.env.downloadpath
 const ytDlpWrap = new YTDlpWrap(yt_dlp);
-//open(`http://localhost:${process.argv[2] || process.env.PORT ||  9999}`);
+open(`http://localhost:${process.argv[2] || process.env.PORT ||  9999}`);
 
 app.use(express.urlencoded({ extended: true }))
 app.set('views', './views')
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 app.use('/src', express.static('src'));
-
-app.get("/",(req,res)=>{
-    res.render("index",{port:process.argv[2] || process.env.PORT ||  9999})
+app.get("/",async(req,res)=>{
+    res.render("index"/*,{port:process.argv[2] || process.env.PORT ||  9999}*/)
 })
 
 app.get("/log",async(req,res)=>{
     datalist = []
     let data =await getAllFiles("public",datalist)
-        res.render("log",{list:data,port:process.argv[2] || process.env.PORT ||  9999})
+        res.render("log",{list:data/*,port:process.argv[2] || process.env.PORT ||  9999*/})
 })
 
 io.sockets.on('connection', function(socket) {
-    socket.emit('message', {'message': 'hello world'});
+    socket.emit('message', {'message': os.platform()});
     socket.on("geturl",(data)=>{
-        console.log(data)
-    let args = ["-4",
+    let args = [
+    "-4",
     "--continue",
     `${data.list}`,
     "--newline",
@@ -44,10 +52,9 @@ io.sockets.on('connection', function(socket) {
     data.url
     ]
     if(data.subtitle!==undefined){args.push(data.subtitle)}
-    if(data.format.split(" ").length!==2 && data.format!=="ba") {args.push("--embed-thumbnail"),args.push("-P public/video/"),args.push(`-f ${data.format}`)}
-    if(data.format==="ba") args.push("-P public/audio/"),args.push(`-f ${data.format}`)
-    if(data.format.split(" ").length===2) {args.push(data.format.split(" ")[0]),args.push(data.format.split(" ")[1]),args.push("-P public/thumbnail/")}
-    console.log(args)
+    if(data.format.split(" ").length!==2 && data.format!=="ba") {args.push("--embed-thumbnail"),args.push(`-P ${downloadpath ||"public/video/"}`),args.push(`-f ${data.format}`)}
+    if(data.format==="ba") args.push(`-P ${downloadpath ||"public/audio/"}`),args.push(`-f ${data.format}`)
+    if(data.format.split(" ").length===2) {args.push(data.format.split(" ")[0]),args.push(data.format.split(" ")[1]),args.push(`-P ${downloadpath || "public/thumbnail/"}`)}
 
     let ytDlpEventEmitter = ytDlpWrap
     .exec(args)
@@ -59,7 +66,7 @@ io.sockets.on('connection', function(socket) {
         socket.broadcast.emit("reloadreq",{"ready":1});
     }
     );
-    console.log(ytDlpEventEmitter.ytDlpProcess.pid);
+    //console.log(ytDlpEventEmitter.ytDlpProcess.pid);
     })
 });
 const getAllFiles = function(dirPath, arrayOfFiles) {
@@ -77,20 +84,3 @@ const getAllFiles = function(dirPath, arrayOfFiles) {
   
     return arrayOfFiles
 }
-/*
-const ytdlp =(url,format,list)=>{
-    let args = ["-4","--continue",`${data.list}`,"--embed-thumbnail","--newline","--embed-subs",'--embed-metadata',"-N 8",`-f ${data.format}`,"-P public/video/","-o %(title)s.%(ext)s",data.url]
-    if(data.format=="ba") args = ["-4","--continue",`${data.list}`,"--newline","-N 8",`-f ${data.format}`,"-P public/audio/","-o %(title)s.%(ext)s",data.url]
-    cp.execFile(yt_dlp,args,async (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error}`)
-            //return error;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-        socket.emit('fromurl',{'error':error,"stdout":stdout,"stderr":stderr});
-        socket.broadcast.emit("reloadreq",{"ready":1});
-        return stdout ? stdout : stderr
-    });
-}
-*/
