4e9e6f3816a173237439c92ce4fc0836d237f269
asd35084591 <70816236+asd35084591@users.noreply.github.com>
2022-05-14 16:57:43 +0800

    add progress bar func

--- src/script.js
+++ src/script.js
@@ -1,37 +1,74 @@
-const appenddata = (data)=>{
-    //stdout
-    if(data.stdout1===undefined && data.stdout2===undefined && data.stderr===undefined) return
-    document.getElementById("stdout")===null?stdout = document.createElement("p"): stdout = document.getElementById("stdout")
-    let mdtext = `[${data.stdout1===undefined?"":data.stdout1}] - ${data?.stdout2===undefined?"":data.stdout2}`
-    let stdouttext = document.createTextNode(mdtext)
-    document.getElementById("stdout").append(stdouttext,document.createElement("br"))
-    stdout.style.backgroundColor = "green"
-    //stderr
-    document.getElementById("stderr")===null?stderr = document.createElement("p"): stderr = document.getElementById("stderr")
-    let stderrtext = document.createTextNode(data.stderr)
-    data.stderr===undefined?"":stderr.append(stderrtext,document.createElement("br"))
-    stderr.style.backgroundColor = "red"
+const appenddata = (data) => {
+	//stdout
+	if (data.stdout1 === undefined && data.stdout2 === undefined && data.stderr === undefined) return;
+	document.getElementById('stdout') === null
+		? (stdout = document.createElement('p'))
+		: (stdout = document.getElementById('stdout'));
+	let mdtext = `[${data.stdout1 === undefined ? '' : data.stdout1}] - ${
+		data?.stdout2 === undefined ? '' : data.stdout2
+	}`;
+
+	let stdouttext = document.createTextNode(mdtext);
+	document.getElementById('stdout').append(stdouttext, document.createElement('br'));
+	stdout.style.backgroundColor = 'green';
+	if (data?.stdout2.split('%')[0] != data?.stdout2) {
+		handleTimelineUpdate(data.stdout2.split('%')[0] + '%');//Progress bar
+		document.getElementById('progress').innerText = `${data.stdout2.split('%')[0] + '%'}`;
+	}
+	//stderr
+	document.getElementById('stderr') === null
+		? (stderr = document.createElement('p'))
+		: (stderr = document.getElementById('stderr'));
+	let stderrtext = document.createTextNode(data.stderr);
+	data.stderr === undefined ? '' : stderr.append(stderrtext, document.createElement('br'));
+	stderr.style.backgroundColor = 'red';
+};
+const timelineContainer = document.querySelector('.timeline-container');
+function handleTimelineUpdate(percent) {
+	timelineContainer.style.setProperty('--progress-position', parseFloat(percent));
 }
-const url = document.getElementById("link");
-const sendreq = (io = "geturl")=>{
-    //let urlreg = new RegExp(/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/).test(url.value)
-    //let urlreg = new RegExp(/http(?:s?):\/\/(?:www\.)?youtu((?:be\.com\/watch\?v=|\.be\/)|(?:be\.com\/playlist\?list=))([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/).test(url.value)
-    let urlreg = new RegExp(/http(?:s?):\/\/(?:www\.)?youtu((?:be\.com\/watch\?v=|\.be\/)|(?:be\.com\/playlist\?list=)|(?:be\.com\/c\/)|(?:be\.com\/channel\/))([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/).test(url.value)
-    if(urlreg===false) {return alert("這不是一個youtube link!")}
-    let videoformat = getradiovalue("videoformat");
-    let playlistvideo = getradiovalue("playlist");
-    let subtitle = getradiovalue("subtitle");
-    let res = socket.emit(io, {'url': document.getElementById("link").value,"format":videoformat,"list":playlistvideo,"subtitle":subtitle});
-    document.getElementById("stdout").innerHTML = "";
-    document.getElementById("stderr").innerHTML = "";
-    /*let data = await fetch(`video?url=${url.value}&format=${videoformat}&list=${playlistvideo}`);
-    let res = await data.text;*/
+
+const url = document.getElementById('link');
+const sendreq = (io = 'geturl') => {
+	//let urlreg = new RegExp(/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/).test(url.value)
+	//let urlreg = new RegExp(/http(?:s?):\/\/(?:www\.)?youtu((?:be\.com\/watch\?v=|\.be\/)|(?:be\.com\/playlist\?list=))([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/).test(url.value)
+	let urlreg = new RegExp(
+		/http(?:s?):\/\/(?:www\.)?youtu((?:be\.com\/watch\?v=|\.be\/)|(?:be\.com\/playlist\?list=)|(?:be\.com\/c\/)|(?:be\.com\/channel\/))([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/
+	).test(url.value);
+	if (urlreg === false) {
+		return alert('這不是一個youtube link!');
+	}
+	let videoformat = getradiovalue('videoformat');
+	let playlistvideo = getradiovalue('playlist');
+	let subtitle = getradiovalue('subtitle');
+	let res = socket.emit(io, {
+		url: document.getElementById('link').value,
+		format: videoformat,
+		list: playlistvideo,
+		subtitle: subtitle,
+	});
+	document.getElementById('stdout').innerHTML = '';
+	document.getElementById('stderr').innerHTML = '';
+};
+const getradiovalue = (name) => {
+	let list = document.getElementsByName(name);
+	for (let i = 0; i < list.length; i++) {
+		if (list[i].checked) {
+			return list[i].value;
+		}
+	}
+};
+function debounce(cb, delay = 1000) {
+	let timeout;
+	return (...args) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			cb(...args);
+		}, delay);
+	};
 }
-const getradiovalue = (name)=>{
-    let list = document.getElementsByName(name);
-    for(let i = 0; i < list.length ; i++){
-        if(list[i].checked){
-            return list[i].value;
-        }
-    }
+document.getElementById('download').addEventListener('click', debounce(reqdeb));
+function reqdeb() {
+	console.log('send');
+	sendreq();
 }
